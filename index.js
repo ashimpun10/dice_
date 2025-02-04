4b90909f1f0e36130416583be2e8678a0e3310f1
Ashim Pun <punashim48@gmail.com>
2025-02-04 07:17:01 +0000

    Remove if else statements

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
 var a=Math.floor(Math.random()*6)+1;
 var b=Math.floor(Math.random()*6)+1;
 
-
-if(a===1){
-    document.querySelector(".img1").setAttribute("src","images/dice1.png");
-}
-else if(a===2){
-    document.querySelector(".img1").setAttribute("src","images/dice2.png");
-}
-else if(a===3){
-    document.querySelector(".img1").setAttribute("src","images/dice3.png");
-}
-else if(a===4){
-    document.querySelector(".img1").setAttribute("src","images/dice4.png");
-}
-else if(a===5){
-    document.querySelector(".img1").setAttribute("src","images/dice5.png");
-}
-else if(a===6){
-    document.querySelector(".img1").setAttribute("src","images/dice6.png");
-}
-
-
-var randomdice="dice"+b+".png";
-document.querySelector(".img2").setAttribute("src","images/"+randomdice);
+document.querySelector(".img1").setAttribute("src","images/dice"+a+".png");
+var c="dice"+b+".png";
+document.querySelector(".img2").setAttribute("src","images/"+c);
 
 if(a>b){
     document.querySelector("h1").innerHTML=" Player1 Wins";
